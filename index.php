452b066bc315d1da5d69002b1d89481d5b86c13a
my <910109610@qq.com>
2020-02-12 13:59:54 +0800

    study

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 get_header();
 $feedurl = array(
 	'https://www.leiue.com/feed',
-	//'https://www.limiwu.com/feed',
-	//'https://www.txcstx.cn/feed.php'
+	'https://www.limiwu.com/feed',
+	'https://www.txcstx.cn/feed.php'
 );//FEED数组
 require_once (ABSPATH.WPINC.'/class-feed.php');
 // $feedin = get_stylesheet_directory().'/feed';
@@ -21,14 +21,25 @@ $feed->enable_order_by_date(false);
 $feed->set_cache_location(get_template_directory_uri().'/feed');//缓存文件夹
 $feed->init();
 $feed->handle_content_type();
-echo('<pre>');
-print_r($feed);
-echo('</pre>');
+// echo('<pre>');
+// print_r($feed);
+// echo('</pre>');
 ?>
 <ul>
 <?php 
 	foreach ($feed->get_items(0,9) as $item){//9是文章篇数
-		echo "<li><a href=".$item->get_permalink().">".$item->get_title()."</a><br>".substr($item->get_description(),0,180)."</li><br>".$item->get_Author().$item->get_date('Y-m-d | g:i a');
+		echo "<li><a href=".$item->get_permalink().">".$item->get_title()."</a><br>".substr($item->get_description(),0,180)."</li><br>".$item->get_date('Y-m-d | g:i a');
+		echo "<br>".$item->get_base();//网址
+		//echo "<br>".print_r($item->get_categories());标签，一个数组格式
+		echo "<br>".$item->get_category()->get_term();//获取第一标签的名字
+		$rss_authors = $item->get_authors();//作者数组
+		//echo "<br>".print_r($item->get_authors());//作者，一个数组格式
+		// foreach ($rss_authors as $key => $value) {
+		// 	echo $value;
+		// }
+		//echo "<br>".print_r($item->get_author());//网址
+		echo "<br>".$item->get_author()->get_name();//获取第一个作者名字
+		echo "<br>".$item->get_permalink();//获取第一个作者名字
 }?>
 </ul>
 
