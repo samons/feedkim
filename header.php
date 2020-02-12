f55b0e0625493f715d5438c390fc5ecb66011943
my <910109610@qq.com>
2020-02-12 21:18:19 +0800

    nav

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<?php wp_head(); ?>
+
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url')?>/style.css">
+
+<script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+
+<!--[if lt IE 9]>
+  <script src="//apps.bdimg.com/libs/html5shiv/3.7/html5shiv.min.js"></script>
+  <script src="//apps.bdimg.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<title><?php if ( is_home() ) {
+        bloginfo('name'); echo "|"; bloginfo('description');
+    } elseif ( is_category() ) {
+        single_cat_title(); echo "|"; bloginfo('name');
+    } elseif (is_single() || is_page() ) {
+        single_post_title();
+    } elseif (is_search() ) {
+        echo "search result:"; echo "|"; bloginfo('name');
+    } elseif (is_404() ) {
+        echo '404 | '.__('您所浏览的页面错误','limiwu');
+    } else {
+		wp_title( '|', true, 'right' ); 
+}?></title>
+
+</head>
+
+<body <?php body_class();?>>
+<?php wp_body_open(); ?>
+
+<?php get_template_part('nav');//顶部导航 ?>
---
+++ nav.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default" id="navbar">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only"><?php bloginfo('name'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand logoImg" href="<?php echo home_url('/');?>"><img alt="Brand" src="<?php bloginfo('template_url'); ?>/image/favicon.ico"></a> 
+      <a class="navbar-brand logoText" href="<?php echo home_url('/');?>"><?php bloginfo('name'); ?></a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <?php 
+        if ( has_nav_menu( 'primary' )) {
+           wp_nav_menu( array(  
+            'theme_location' => 'primary',  
+            'depth' => 2,   
+            'container' => false,   
+            'menu_class' => 'nav navbar-nav navbar-right',   
+            'fallback_cb' => 'wp_page_menu')   
+            );
+        }
+      ?>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
