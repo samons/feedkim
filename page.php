d5cd4e9e08f58157cc5269ff7f98bcbefa555987
xie <910109610@qq.com>
2020-04-25 10:19:08 +0800

    menu

--- page.php
+++ page.php
@@ -13,19 +13,7 @@ error_reporting(E_ALL ^ (E_WARNING|E_NOTICE));// 屏蔽域名不存在等访问�
 				<?php //RSS源，对应的是feeds菜单
 				if ( has_nav_menu('feeds')) { ?>
 				<form method="POST" action="<?php echo home_url('/');?>" role="form">
-				<?php 
-					wp_nav_menu( array(  
-					'theme_location' => 'feeds',
-					'container'  => 'div',
-					'container_id'  => 'feeds-div',
-					'container_class' => 'feeds-div',
-					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-					'menu_class' => 'feed-menu',
-				),); ?>
-				<!-- 图标地址，获取用，不显示 -->
-				<span class="display" id="myico"><?php bloginfo('template_url'); ?>/image/favicon.ico</span>
-				<!-- input，获取用，不显示 -->
-				<input class="display" type="text" name="feedUrl" value="">
+					<?php require_once('feeds-menu-tree.php');?>
 				</form>
 				<?php } ?>
 			</div>
