df383f17b3d739cfb8449757abe71238ec900d2a
zitonglu <910109610@qq.com>
2020-04-26 11:13:59 +0800

    nav

--- nav.php
+++ nav.php
@@ -15,20 +15,38 @@
       <?php //RSS源，对应的是feeds菜单
         if ( has_nav_menu('feeds')) { ?>
         <form method="POST" action="<?php echo home_url('/');?>" role="form" class=" visible-xs">
-        <?php 
-          wp_nav_menu( array(  
-          'theme_location' => 'feeds',
-          'container'  => 'div',
-          'container_id'  => 'feeds-div',
-          'container_class' => 'feeds-div',
-          'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-          'menu_class' => 'feed-menu',
-        ),); ?>
-        <!-- 图标地址，获取用，不显示 -->
-        <span class="display" id="myico"><?php bloginfo('template_url'); ?>/image/favicon.ico</span>
-        <!-- input，获取用，不显示 -->
-        <input class="display" type="text" name="feedUrl" value="">
-        <hr>
+          <ul class="nav navbar-nav">
+          <?php
+          global $wpdb;
+          $table_wp_posts = $wpdb->prefix . "posts";
+          $table_wp_postmeta = $wpdb->prefix . "postmeta";
+
+          $sql = "SELECT `ID`,`post_title` FROM ".$table_wp_posts." WHERE `post_type` = 'nav_menu_item' AND `post_title` <> ''";
+          $results = $wpdb->get_results($sql);
+          for ($i=0; $i<count($results); $i++) { 
+            $nav_ID = $results[$i]->ID;
+            $nav_title = $results[$i]->post_title;
+            $nav_IDtoTitle[$nav_ID] = $nav_title;//单数组，结构{ID=>title}
+          }
+
+          $sql = "SELECT * FROM ".$table_wp_postmeta." WHERE `meta_key` = '_menu_item_url' AND `meta_value` != ''";
+          $results = $wpdb->get_results($sql);
+          for ($i=0; $i<count($results); $i++) {
+            $nav_ID = $results[$i]->post_id;
+            $nav_url = $results[$i]->meta_value;
+            $nav_IDtoUrl[$nav_ID] = $nav_url;//单数组，结构{ID=>url}
+          }
+          $nav_IDtoUrl = array_unique($nav_IDtoUrl);//删除重复项
+
+          //关闭数据库
+          $wpdb->flush();
+
+          foreach ($nav_IDtoUrl as $key => $value) {
+            echo '<li><button type="submit" class="btn btn-link" name="feedbutton" value="'.$value.'">'.$nav_IDtoTitle[$key].'</button></li>';
+          }
+
+          ?>
+          </ul>
         </form>
       <?php }//RSS-end ?>
       <?php   
